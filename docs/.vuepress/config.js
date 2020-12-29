10ec0505d7606ad69cdb92313cce182e2946a603
fizer-li <kevin_fangfang@163.com>
2020-12-29 08:35:26 +0800

    自定义组件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,20 @@
 const nav = require('./config/nav.js')
-
+const moment = require('moment');
 module.exports = {
   title: "fizer",
-  
+  base: "/myBlog/",
+  plugins: [
+    [
+      '@vuepress/last-updated',
+      {
+        transformer: (timestamp, lang) => {
+         
+          moment.locale(lang)
+          return moment(timestamp).format('YYYY-MM-DD')
+        }
+      }
+    ]
+  ],
   themeConfig: {
     logo: '/assets/img/pear.jpg',
     sidebarDepth: 2,
