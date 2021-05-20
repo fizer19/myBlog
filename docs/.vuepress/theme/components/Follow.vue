2e24c33266ea3cf131705ed9d3a527fc43750b23
fizer-li <kevin_fangfang@163.com>
2021-05-20 16:49:51 +0800

    update

--- docs/.vuepress/theme/components/Follow.vue
+++ docs/.vuepress/theme/components/Follow.vue
@@ -33,7 +33,7 @@ export default {
           link: "https://juejin.cn/user/720899266583527",
           src: "/myBlog/assets/icons/juejin.png",
         },
-        { name: "留言", link: "/message/message.html", src: "/myBlog/assets/icons/message.png" },
+        // { name: "留言", link: "/myBlog/message/message.html", src: "/myBlog/assets/icons/message.png" },
       ],
     };
   },
